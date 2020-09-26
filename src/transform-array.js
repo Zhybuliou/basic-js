27b735ea5854c43d5a2835ef057cab0a28ab6dd2
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2020-09-27 01:14:25 +0300

    docs: Task Transform Array 70% complite

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+module.exports = function transform( arr ) {
+
   // remove line with error and write your code here
+  let output = [];
+  f = 0;
+
+    for( let z = 0; z < arr.length; z++){
+      if( arr[z] === '--discard-prev'){
+           output[f] != arr[z];
+           output.splice( z - 1, 2);
+      }
+      else if( arr[z] === '--discard-next'){
+           output[f] != arr[z];
+           output.splice(z,2);
+  
+           
+      }
+      else if( arr[z] === '--double-prev'){
+           output[f] != arr[z];
+           output[f] = arr[z-1];
+      }
+      else if( arr[z] === '--double-next'){
+           output[f] != arr[z];
+           output[f] = arr[z+1];
+      }
+      else{output[f] = arr[z];}
+      f++;
+    }
+    return output.filter(function(element){
+       return element !== undefined;
+      });
+
+    
 };
