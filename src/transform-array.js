dadc6a9d9724d1697aee5d4f88280aa04e3ee95e
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2020-09-27 13:35:52 +0300

    docs: =( not yet

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,36 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform( arr ) {
-
-  // remove line with error and write your code here
-  let output = [];
-  f = 0;
-
-    for( let z = 0; z < arr.length; z++){
-      if( arr[z] === '--discard-prev'){
-           output[f] != arr[z];
-           output.splice( z - 1, 2);
-      }
-      else if( arr[z] === '--discard-next'){
-           output[f] != arr[z];
-           output.splice(z,2);
   
-           
-      }
-      else if( arr[z] === '--double-prev'){
-           output[f] != arr[z];
-           output[f] = arr[z-1];
-      }
-      else if( arr[z] === '--double-next'){
-           output[f] != arr[z];
-           output[f] = arr[z+1];
-      }
-      else{output[f] = arr[z];}
-      f++;
-    }
-    return output.filter(function(element){
-       return element !== undefined;
-      });
+ // remove line with error and write your code here
+ let output = [];
+ f = 0;
+
+   for( let z = 0; z < arr.length; z++){
+     if( arr[z] === '--discard-prev'){
+          output[f] != arr[z];
+          output.splice( z - 1, 2);
+     }
+     else if( arr[z] === '--discard-next'){
+          output[f] != arr[z];
+          output.splice(z,2);
+ 
+          
+     }
+     else if( arr[z] === '--double-prev'){
+          output[f] != arr[z];
+          output[f] = arr[z-1];
+     }
+     else if( arr[z] === '--double-next'){
+          output[f] != arr[z];
+          output[f] = arr[z+1];
+     }
+     else{output[f] = arr[z];}
+     f++;
+   }
+   return output.filter(function(element){
+      return element !== undefined;
+     });
+  
 
     
 };
