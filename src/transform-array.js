5edade2da61001b63bd074e07e989b6535f90a21
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2020-09-30 02:07:58 +0300

    docs: Transform array =(

--- src/transform-array.js
+++ src/transform-array.js
@@ -4,34 +4,36 @@ module.exports = function transform( arr ) {
   
  // remove line with error and write your code here
  let output = [];
- f = 0;
-
+ 
+  if(Array.isArray(arr)){
    for( let z = 0; z < arr.length; z++){
-     if( arr[z] === '--discard-prev'){
-          output[f] != arr[z];
-          output.splice( z - 1, 2);
-     }
-     else if( arr[z] === '--discard-next'){
-          output[f] != arr[z];
-          output.splice(z,2);
+     if( arr[z] === '--discard-next'){
+          if(((arr[z + 2]) !== '--discard-prev') || ((arr[z + 2]) !== '--double-prev')){
+               z += 2;
+          }else{ z += 2;}
  
           
      }
-     else if( arr[z] === '--double-prev'){
-          output[f] != arr[z];
-          output[f] = arr[z-1];
+     else if( arr[z] === '--discard-prev'){
+          output.pop();
      }
      else if( arr[z] === '--double-next'){
-          output[f] != arr[z];
-          output[f] = arr[z+1];
+          if( arr[z + 1] !== undefined){
+          output.push(arr[z + 1]);
      }
-     else{output[f] = arr[z];}
-     f++;
-   }
-   return output.filter(function(element){
-      return element !== undefined;
-     });
-  
+     }
+     else if( arr[z] === '--double-prev'){
+           if( arr[z - 1] !== undefined){
+          output.push(arr[z-1]);}
+     }
 
+     else{output.push(arr[z]);}
+
+   }
+   return output;
+}
+else{
+     throw new Error();
+}
     
 };
