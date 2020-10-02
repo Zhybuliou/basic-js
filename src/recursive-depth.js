d447c91c413ec466f269254e6adca63e8d98008b
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2020-10-02 09:48:46 +0300

    docs: done task depth array

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,11 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(arr) {
-     
-    if (Array.isArray(arr)) return 1 + Math.max(...arr.map(t => getArrayDepth(t)))
-    else return 0
   
-    }
-
+  calculateDepth(arr, level = 1) {
+    let nArray = [];
+    if(Array.isArray(arr)){
+     for(let i = 0; i < arr.length; i++){
+      if(Array.isArray(arr[i])){
+        nArray = arr.flat();
+        return this.calculateDepth(nArray,level+1);
+      }
+      
+     }
+     return level;
+    }  
+}
 };
