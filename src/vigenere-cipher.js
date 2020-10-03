5b3214a4ca99cb22957bfb99031fd94facae3bec
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2020-10-04 01:33:36 +0300

    docs: done vigenere task 90%

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,57 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    let direct = true;
+    let reverse = false;
   }
-}
+  encrypt(text, key,done = 1) {
+    // remove line with error and write your code here
+    let result = '';
+    let keys = key.toUpperCase();
+    let go = text.toUpperCase();
+
+    for(let i = 0, j = 0; i < go.length; i++,j++){
+      if (j >= keys.length) j = 0;
+      if( 65 <= go.charCodeAt(i) && go.charCodeAt(i) <= 90 ){
+              let sum = go.charCodeAt(i) + keys.charCodeAt(j)-65;
+              if( sum > 90){
+                result += String.fromCharCode(sum-26);
+              }else{
+                result += String.fromCharCode(sum);
+              }
+      }else{
+        result += String.fromCharCode(go.charCodeAt(i));
+        j--;
+      }
+    }
+    return result;
+    
+  }    
+  decrypt(text, key,done = -1) {
+    // remove line with error and write your code here
+    let result = '';
+    let keys = key.toUpperCase();
+    let go = text.toUpperCase();
+
+    for(let i = 0, j = 0; i < go.length; i++,j++){
+      if (j >= keys.length) j = 0;
+      if( 65 <= go.charCodeAt(i) && go.charCodeAt(i) <= 90 ){
+              let sum = go.charCodeAt(i) - keys.charCodeAt(j)+65;
+              if( sum < 65){
+                result += String.fromCharCode(sum+26);
+              }else{
+                result += String.fromCharCode(sum);
+              }
+      }else{
+        result += String.fromCharCode(go.charCodeAt(i));
+        j--;
+      }
+    }
+    return result;
+   
+    }
+
+  }
 
 module.exports = VigenereCipheringMachine;
